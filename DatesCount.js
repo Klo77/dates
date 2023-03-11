90aee9c7c32c06a79daf0c4eb6e1f6e2ee3da900
Khalid AL-Fawaz <107946457+Klo77@users.noreply.github.com>
2023-03-11 22:49:56 +0300

    Add files via upload

---
+++ DatesCount.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Row , Col} from 'react-bootstrap';
+const DatesCount = ({person})=>{
+    return (
+        <Row className='justify-content-center'>
+        <Col sm="8" className="">
+            لديك {person.length} مواعيد اليوم
+        </Col>
+    </Row>  
+    )
+}
+export default DatesCount;
---
+++ data.js
@@ -0,0 +1,8 @@
+export const person = [
+    {id:1 , name : "محمد", date:'4 مساء', img:'صورة'},
+    {id:2 , name : "نواف", date:'4 مساء', img:'صورة'},
+    {id:3 , name : "خالد", date:'4 مساء', img:'صورة'},
+    {id:4 , name : "علي", date:'4 مساء', img:'صورة'},
+    {id:5 , name : "جهاد", date:'4 مساء', img:'صورة'},
+
+]
